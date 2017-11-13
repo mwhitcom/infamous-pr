972f95e0314950bcf61bd9cb3195ce7b8f6cde54
Matt Whitcomb <mwhitcom@gmail.com>
2017-11-13 13:51:31 -0800

    styling for client thumbnails

--- src/components/SingleClient.js
+++ src/components/SingleClient.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SingleClient extends Component {
   render() {
     return (
       <div className="single-client-container">
-        Hello
+        <Link className="client-link" to="/client/client-name">
+          <div className="client-thumbnail-container">
+            <img className="client-thumbnail" src="https://ichef.bbci.co.uk/images/ic/1200x675/p02xxgn9.jpg" alt="Client Name"/>
+          </div>
+          <h1 className="client-name">Pete Tong</h1>
+        </Link>
       </div>
     );
   }
