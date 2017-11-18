31937c12a17fec939c84160182ccc1e13861ca59
Matt Whitcomb <mwhitcom@gmail.com>
2017-11-18 10:22:55 -0800

    created fake model

---
+++ model.js
@@ -0,0 +1,45 @@
+// Client page displays image and name of all clients
+// outlet_logo in newsStory pulls from an outletLogo library we will store
+
+// Many newsStory
+// Many client
+// 1 outletLogos
+// 1 websiteInfo
+
+const newsStory = {
+  news_image: 'image.jpg',
+  outlet_logo: 'logo.jpg',
+  news_title: 'text',
+  dek: 'text',
+  news_link: 'url',
+  tags: ['text', 'text']
+};
+
+const client = {
+  name: 'text',
+  type: ['artist', 'event', 'etc'],
+  image: 'image.jpg',
+  bio: 'long text',
+  facebook_link: 'url',
+  twitter_link: 'url',
+  insta_link: 'url',
+  youtube_link: 'url',
+  soundcloud_link: 'url',
+  website_link: 'url',
+  press_kit: 'file to download',
+  news_list: ['tagged stories', 'tagged stories']
+};
+
+const outletLogos = {
+  name: 'text',
+  image: 'image.jpg'
+};
+
+const websiteInfo = {
+  dsp_text: 'long text',
+  about_text: 'long text',
+  deck: 'pdf to view / download',
+  landing_video: 'video.mov',
+  address: 'text',
+  password: 'text'
+};
