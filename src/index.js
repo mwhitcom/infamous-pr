86c4f942ca07f7c8399bdae94243cc54a790e091
Matt Whitcomb <mwhitcom@gmail.com>
2017-11-25 13:23:26 -0800

    created login page with styling

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import rootReducer from './reducers/index.js'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index.js';
 
-import reducers from './reducers';
 import Landing from './components/Landing_News/Landing';
 import Clients from './components/Client_List/Clients';
 import About from './components/About/About';
 import DSP from './components/DSP/DSP';
 import ClientPage from './components/Client/ClientPage';
+import Login from './components/Admin/Login';
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,6 +25,7 @@ ReactDOM.render(
           <Route path="/about" component={About} />
           <Route path="/dsp" component={DSP} />
           <Route path="/clients" component={Clients} />
+          <Route path="/login" component={Login} />
           <Route path="/" component={Landing} />
         </Switch>
       </div>
