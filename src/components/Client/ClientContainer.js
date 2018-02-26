23a0130de4994754cef05c096a13f7b00499c2ec
mwhitcom <mwhitcom@gmail.com>
2018-02-25 22:32:29 -0800

    connected database to clients page, hooked up client filter

--- src/components/Client/ClientContainer.js
+++ src/components/Client/ClientContainer.js
@@ -6,7 +6,7 @@ import SingleClient from './SingleClient';
 export default function ClientContainer(props) {
   const clients = props.list.map(client => <SingleClient name={client.name} image={client.image} />);
   return (
-    <div styleName={'container'}>
+    <div style={props.style} styleName={'container'}>
       <h1 styleName={'title'}>{props.type}</h1>
       <div styleName={'client-grid'}>
         {clients}
