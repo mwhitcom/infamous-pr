258d03ef0a295d6d2a3930c187c024333b5cfd68
mwhitcom <mwhitcom@gmail.com>
2018-03-29 18:33:58 -0700

    fixed news bug on client page

--- src/components/SingleClient/SingleClientNews.js
+++ src/components/SingleClient/SingleClientNews.js
@@ -7,14 +7,14 @@ export default function SingleClientNews(props) {
   return (
     <div styleName={'story-container'}>
       <div styleName={'image-container'}>
-        <a href={story.news_link} target="_blank">
+        <a href={story.news_link.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%')} target="_blank">
           <img src={story.image.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%2F')} alt="news image" />
         </a>
       </div>
-      <a href={story.news_link} target="_blank">
+      <a href={story.news_link.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%')} target="_blank">
         <ul styleName={'content-list'}>
           <li styleName={'date'}>{story.date} - {story.outlet}</li>
-          <li styleName={'title'}>{story.title}</li>
+          <li styleName={'title'}>{story.title.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%')}</li>
         </ul>
       </a>
     </div>
