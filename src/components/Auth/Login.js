b1a3c2c6f42f21ecbaf76199ebc0cca965ecf0fe
mwhitcom <mwhitcom@gmail.com>
2018-05-10 01:03:43 -0700

    updated case sensitive css filenames

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+import '../../utils/fire'
+
+import './login.css';
+
+const uiConfig = {
+  signInSuccessUrl: '/admin',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccess: function(currentUser, credential, redirectUrl) {
+      sessionStorage.setItem('token', 'true');
+      return true;
+    },
+  }
+};
+
+const Login = () => {
+  return (
+    <div styleName="container">
+      <img src="/assets/images/infamous_logo_black.png" alt="Infamous" />
+      <StyledFirebaseAuth 
+        uiCallback={ui => ui.disableAutoSignIn()} 
+        uiConfig={uiConfig} 
+        firebaseAuth={firebase.auth()}
+      />
+    </div>
+  );
+}
+
+export default Login;
