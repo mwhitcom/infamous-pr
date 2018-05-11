a6281ec5ed592d32712ad2940bc28bc4aed24560
mwhitcom <mwhitcom@gmail.com>
2018-05-11 10:46:39 -0700

    fixed image loading styles in single client page

--- src/components/SingleClient/topBlock/TopBlock.js
+++ src/components/SingleClient/topBlock/TopBlock.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './topBlock.css';
 
-const TopBlock = (props) => {
-  const { data } = props;
-  const image = data && data.image.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%2F');
+class TopBlock extends Component {
+  state = {
+    loaded: false
+  }
 
-  return (
-    <div styleName="image-container">
-      <h1 styleName="client-title">{data.name}</h1>
-      <img src={image} alt={data.name}/>
-    </div>
-  );
+  handleLoad = () => {
+    this.setState({ loaded: true })
+  }
+
+  render() {
+    const { data } = this.props;
+    const { loaded } = this.state;
+    const style = loaded ? { maxHeight: '100vh' } : { height: '100vh' }
+    const image = data && data.image.replace(/@/g, '=').replace(/~/g, '&').replace(/!/g, '%2F');
+  
+    return (
+      <div styleName="image-container" style={style}>
+        <h1 styleName="client-title">{data.name}</h1>
+        <img src={image} alt={data.name} onLoad={this.handleLoad}/>
+      </div>
+    );
+  }
 }
 
 export default TopBlock;
