35f21f3e21a9bc0d9ff5e1d4710d5fa1bb014451
Spencer Charest <SpencerC77@gmail.com>
2018-05-10 20:05:35 -0700

    refactored twitter cloud function and POST

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,23 @@
 const admin = require('firebase-admin')
+const express = require('express')
+const cors = require('cors')
 const functions = require('firebase-functions')
-const creds = require('./firebase_creds.json')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 const generateThumbnail = require('./image/generateThumbnail')
 
-const postTwitter = require('./social/post_twitter');
+const twitter = require('./social/twitter');
 
 admin.initializeApp(functions.config().firebase);
 
-// SOCIAL POSTING
-exports.postTwitter = functions.https.onRequest(postTwitter);
+app.post('/twitter', twitter)
 
 // IMAGE RESIZE
 exports.generateThumbnail = functions.storage.object().onChange(generateThumbnail);
 
+exports.api = functions.https.onRequest(app)
